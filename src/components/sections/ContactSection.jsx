78fb903d449471431c6d068f633f1c12120c3026
Mohammad-Yassine-eng <2002m.yassine@gmail.com>
2025-07-19 22:13:51 +0300

    selection class

--- src/components/sections/ContactSection.jsx
+++ src/components/sections/ContactSection.jsx
@@ -1,8 +1,11 @@
-import { Github, Linkedin, Instagram, Phone } from 'lucide-react';
 import ContactForm from '@/components/sections/ContactForm';
+import { FaGithub, FaWhatsapp, FaLinkedin, FaInstagram } from 'react-icons/fa';
 
 export default function ContactSection({ lang }) {
   const isAr = lang === 'ar';
+  const message = isAr
+    ? 'مرحبًا م. محمد ياسين، رأيت موقعك الشخصي وأرغب بالتواصل معك بشأن مشروع 🌟'
+    : 'Hi Eng. Mohammad Yassine, I visited your website and would like to discuss a project 🌟';
 
   return (
     <section id="contact" className="w-full px-6 py-20 bg-background text-text max-w-7xl mx-auto scroll-mt-20">
@@ -12,17 +15,21 @@ export default function ContactSection({ lang }) {
 
       {/* روابط التواصل */}
       <div className="flex justify-center gap-6 mb-10 text-accent text-2xl">
-        <a href="https://github.com/Yassinov7" target="_blank" rel="noopener noreferrer">
-          <Github />
+        <a href="https://github.com/Yassinov7" target="_blank" rel="noopener noreferrer"  className="hover:text-yellow-400">
+          <FaGithub size={28}/>
         </a>
-        <a href="https://www.linkedin.com/in/mohammed-yassine-70499921a" target="_blank" rel="noopener noreferrer">
-          <Linkedin />
+        <a href="https://www.linkedin.com/in/mohammed-yassine-70499921a" target="_blank" rel="noopener noreferrer"  className="hover:text-blue-500">
+          <FaLinkedin size={28}/>
         </a>
-        <a href="https://wa.me/963935787445" target="_blank" rel="noopener noreferrer">
-          <Phone />
+        <a
+          href={`https://wa.me/963935787445?text=${encodeURIComponent(message)}`}
+          target="_blank"
+          rel="noopener noreferrer" className="hover:text-green-500"
+        >
+          <FaWhatsapp size={28} />
         </a>
-        <a href="https://instagram.com/mhmmdyassine" target="_blank" rel="noopener noreferrer">
-          <Instagram />
+        <a href="https://instagram.com/mhmmdyassine" target="_blank" rel="noopener noreferrer"  className="hover:text-[#E1306C]">
+          <FaInstagram size={28}/>
         </a>
       </div>
 
